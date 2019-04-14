cca375582942e93244e8bc24e013a089018a9a26
Алина Щербинина <48168698+alina-shcherbinina@users.noreply.github.com>
2019-04-14 14:32:10 +0800

    Update and rename ConsoleApplication33.cpp to sumofdigits.cpp

---
+++ sumofdigits.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int Summ(int64_t a) {
+	if (a < 10)
+		return a;
+	if (a >= 10)
+		return (Summ(a / 10)+ a % 10);;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int64_t a;
+	cout << "Введите число: ";
+	cin >> a;
+	cout << "ответ: " << Summ(a);
+	return 0;
+}
+
